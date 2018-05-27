c295e84948c3edf1ecab88971800ffa41be1a472
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-05-27 20:01:36 +0530

    Created a comment broadcast, configured the websocket route, and updated the cocmment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   
-  resources :comments
+ # resources :comments   Dont need it because we are using actiocable
   devise_for :users
   resources :blogs
   root to: 'pages#home' 
   get 'pages/about'
+
+
+  mount ActionCable.server => '/cable'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
